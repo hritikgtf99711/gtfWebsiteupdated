bb0901423ec5c86e14df2e6cea40c567ea11bf1a
Hritik <hritik.kumar@gtftechnologies.com>
2025-11-06 16:25:04 +0530

    running video

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,35 @@
+// app/layout.js
 "use client";
-import { createPortal } from "react-dom"; // Import createPortal
-import SparkleBackground from "./components/SparkleBackground";
-import "./globals.css";
-import Header from "./components/Home/Header";
-import Footer from "./components/Home/Footer";
-import { oswald } from "./utils/font";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import Sidemenu from "./components/Sidemenu/Sidemenu";
-export default function RootLayout({ children }) {
-  const location = usePathname();
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  return (
-    <html lang="en" className={oswald.variable}>
-      <body className="overflow-x-hidden ">
-        <div id="smooth-wrapper">
-      <div id="smooth-content">
-        {mounted && <SparkleBackground />}
-        </div>
-        </div>
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-        <div>
-          {location == "/" && <Header />} {children}
-          <Footer />
+gsap.registerPlugin(ScrollSmoother,ScrollTrigger );
+
+export default function RootLayout({ children }) {
+  useEffect(() => {
+
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 2,
+      normalizeScroll:true
+    });
+
+    
+    // Refresh on resize
+    
+  }, []);
+
+  return (
+    <html lang="en">
+      <body>
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            {children}
+          </div>
         </div>
-        <Sidemenu/>
       </body>
     </html>
   );
